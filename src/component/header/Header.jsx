b6d68d7c1ddc7f760a3fa40da1e3b2ae5b917829
dmitry semenov <dmitrysemenov@Mac-mini-Dmitry.local>
2025-01-20 11:50:51 +0300

    added test header

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,13 @@
+import AppBar from '@mui/material/AppBar';
+
+
+const Header = () => {
+    return (
+        <>
+            <AppBar>Header</AppBar>
+        </>
+    );
+}
+
+
+export default Header;
