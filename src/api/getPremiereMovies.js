38c2c6064fbcfd495562a6eac6b61827ef70e0f8
dmtr <ddmtry064@gmail.com>
2025-01-25 12:27:46 +0300

    feat: added new request to api

---
+++ src/api/getPremiereMovies.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+// const KEY = import.meta.env.VITE_KINOPOISK_API_KEY;
+// const URL = import.meta.env.VITE_KINOPOISK_PREMIER_MOVIES;
+
+export const getPremiereMovies = async () => {
+  const options = {
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": "7cabb1b4-98e5-4ac1-a19c-8d6ccf0f5fef",
+    },
+  };
+  const response = await axios.get(
+    `https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2025&month=JANUARY`,
+    options,
+  );
+  return response.data.items;
+};
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -8,107 +8,99 @@ import ClipLoader from "react-spinners/ClipLoader";
 import { BeatLoader } from "react-spinners";
 
 const Main = () => {
-    const [movies, setMovies] = useState([]);
-    const [title, setTitle] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [title, setTitle] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        // setTimeout(() => {
-        //     setLoading(!loading);
-        // }, 2000);
-        // let randomPage = Math.random() * (10500 - 1) + 1;
-        //
-        // const fetchData = async () => {
-        //     const data = await getMovies(randomPage);
-        //     setMovies(data.docs);
-        // };
-        // fetchData();
-    }, []);
+  useEffect(() => {
+    // setTimeout(() => {
+    //     setLoading(!loading);
+    // }, 2000);
+    // let randomPage = Math.random() * (10500 - 1) + 1;
+    //
+    // const fetchData = async () => {
+    //     const data = await getMovies(randomPage);
+    //     setMovies(data.docs);
+    // };
+    // fetchData();
+  }, []);
 
-    const handleInput = async () => {
-        // setTimeout(() => {
-        //     setLoading(!loading);
-        // }, 1000);
-        // setLoading(!loading);
-        const data = await getMovieByTitle(title);
-        setMovies(data.docs);
-    };
+  const handleInput = async () => {
+    // setTimeout(() => {
+    //     setLoading(!loading);
+    // }, 1000);
+    // setLoading(!loading);
+    const data = await getMovieByTitle(title);
+    setMovies(data.docs);
+  };
 
-    return (
-        <>
-            <main className={styles.container}>
-                <section className={styles.main}>
-                    <div className={styles.main__search}>
-                        <div className={styles["main__search-wrapper"]}>
-                            <input
-                                className={styles["main__search-field"]}
-                                type="text"
-                                placeholder={"Название фильма"}
-                                value={title}
-                                onChange={(event) => {
-                                    setTitle(event.target.value);
-                                }}
-                            />
-                            <button
-                                className={styles["main__search-button"]}
-                                type={"submit"}
-                                onClick={handleInput}
-                            >
-                                Найти
-                            </button>
-                        </div>
+  return (
+    <>
+      <main className={styles.container}>
+        <section className={styles.main}>
+          <div className={styles.main__search}>
+            <div className={styles["main__search-wrapper"]}>
+              <input
+                className={styles["main__search-field"]}
+                type="text"
+                placeholder={"Название фильма"}
+                value={title}
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                }}
+              />
+              <button
+                className={styles["main__search-button"]}
+                type={"submit"}
+                onClick={handleInput}
+              >
+                Найти
+              </button>
+            </div>
 
-                        <DensityMediumIcon sx={{ height: 50 }} />
-                    </div>
+            <DensityMediumIcon sx={{ height: 50 }} />
+          </div>
 
-                    <div>
-                        {/*<Typography>*/}
-                        {/*    <h2>Топ 10 популярных фильмов</h2>*/}
-                        {/*</Typography>*/}
-                    </div>
-                    <div className={styles.main__loading}>
-                        <BeatLoader loading={loading} />
-                    </div>
+          <div>
+            <Typography>
+              <h2>Премьеры в январе 2025</h2>
+            </Typography>
+          </div>
+          <div className={styles.main__loading}>
+            <BeatLoader loading={loading} />
+          </div>
 
-                    <div className={styles.main__items}>
-                        {movies.map((m) => (
-                            <Card
-                                className={styles["main_items-item"]}
-                                key={m.id}
-                                sx={{ width: 220 }}
-                            >
-                                <CardContent>
-                                    <Box component={"img"} src={m.poster.url} />
+          <div className={styles.main__items}>
+            {movies.map((m) => (
+              <Card
+                className={styles["main_items-item"]}
+                key={m.id}
+                sx={{ width: 220 }}
+              >
+                <CardContent>
+                  <Box component={"img"} src={m.poster.url} />
 
-                                    <Typography component={"div"}>
-                                        <b>
-                                            {m.name === null
-                                                ? m.alternativeName
-                                                : m.name}
-                                        </b>
-                                    </Typography>
-                                    <Typography>
-                                        {m.year === null
-                                            ? "Год неизвестен"
-                                            : m.year + " г."}
-                                    </Typography>
+                  <Typography component={"div"}>
+                    <b>{m.name === null ? m.alternativeName : m.name}</b>
+                  </Typography>
+                  <Typography>
+                    {m.year === null ? "Год неизвестен" : m.year + " г."}
+                  </Typography>
 
-                                    {m.genres.map((g) => (
-                                        <Typography color="blue">
-                                            {g.name}
-                                        </Typography>
-                                    ))}
-                                    <button className={styles["favorite-btn"]}>
-                                        ♥
-                                    </button>
-                                </CardContent>
-                            </Card>
-                        ))}
-                    </div>
-                </section>
-            </main>
-        </>
-    );
+                  {m.genres.map((g) => (
+                    <Typography color="blue">{g.name}</Typography>
+                  ))}
+                  {/*<button className={styles["favorite-btn"]}>*/}
+                  {/*    ♥*/}
+                  {/*</button>*/}
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Main;
