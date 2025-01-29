2d2c39ccb13b3626478d3240a2e844a745e59c70
Dmitry Semenov <ddmtry064@gmail.com>
2025-01-29 11:13:29 +0300

    chore: react hook form added

--- src/components/Login-form/LoginForm.jsx
+++ src/components/Login-form/LoginForm.jsx
@@ -1,69 +1,83 @@
-import styles from "./LoginForm.module.css";
-import { Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import styles from './LoginForm.module.css';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useForm } from 'react-hook-form';
 
 const LoginForm = () => {
-    return (
-        <div className={styles.hhh}>
-            <div className={styles.back}>
-                <Link to="/">
-                    <button className={styles.back__button}>
-                        <ArrowBackIcon />
-                    </button>
-                </Link>
-            </div>
+  const { register, handleSubmit, formState } = useForm({ mode: 'onchange' });
+  const onSubmit = (data) => {};
 
-            <div className={styles.wrapper}>
-                <form className={styles.form}>
-                    <Typography>
-                        <h1 className={styles.form__title}>Вход</h1>
-                    </Typography>
+  const emailError = formState.errors['email']?.message;
 
-                    <div className={styles["form__input-box"]}>
-                        <input
-                            className={styles["form__input-field"]}
-                            type="text"
-                            placeholder={"Почта или имя пользователя"}
-                            required
-                        />
-                    </div>
-                    <div className={styles["form__input-box"]}>
-                        <input
-                            className={styles["form__input-field"]}
-                            type="password"
-                            placeholder={"Пароль"}
-                            required
-                        />
-                    </div>
+  return (
+    <div className={styles.hhh}>
+      <div className={styles.back}>
+        <Link to="/">
+          <button className={styles.back__button}>
+            <ArrowBackIcon />
+          </button>
+        </Link>
+      </div>
 
-                    <div className={styles.form__remember}>
-                        <Typography>
-                            <label htmlFor="">
-                                <input type="checkbox" />
-                                Запомнить меня
-                            </label>
-                        </Typography>
-                        <a>
-                            <Typography>Забыл пароль</Typography>
-                        </a>
-                    </div>
+      <div className={styles.wrapper}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <Typography>
+            <h1 className={styles.form__title}>Вход</h1>
+          </Typography>
 
-                    <button className={styles.form__button} type="submit">
-                        Войти
-                    </button>
+          <div className={styles['form__input-box']}>
+            <input
+              {...register('email', {
+                required: true,
+                maxLength: 55,
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Неверный формат почты',
+                },
+              })}
+              className={styles['form__input-field']}
+              placeholder={'Почта'}
+              type="email"
+            />
+          </div>
+          {emailError && <p>{emailError}</p>}
+          <div className={styles['form__input-box']}>
+            <input
+              className={styles['form__input-field']}
+              type="password"
+              placeholder={'Пароль'}
+              required
+            />
+          </div>
 
-                    <div className={styles.form__register}>
-                        <Typography>
-                            <p>
-                                Первый раз здесь? <a href="">Регистрация</a>
-                            </p>
-                        </Typography>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+          <div className={styles.form__remember}>
+            <Typography>
+              <label htmlFor="">
+                <input type="checkbox" />
+                Запомнить меня
+              </label>
+            </Typography>
+            <a>
+              <Typography>Забыл пароль</Typography>
+            </a>
+          </div>
+
+          <button className={styles.form__button} type="submit">
+            Войти
+          </button>
+
+          <div className={styles.form__register}>
+            <Typography>
+              <p>
+                Первый раз здесь? <a href="">Регистрация</a>
+              </p>
+            </Typography>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
