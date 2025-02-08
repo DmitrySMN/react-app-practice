59e63c89767c2f61e54b7903b3b71a161ff9cee0
dmtr <ddmtry064@gmail.com>
2025-02-08 16:11:12 +0300

    feat: add similar movies to movie details

--- src/components/Movie-card/MovieCard.jsx
+++ src/components/Movie-card/MovieCard.jsx
@@ -1,54 +1,57 @@
 import styles from './MovieCard.module.css';
 import { Box, Card, CardContent, Typography, Button } from '@mui/material';
 import { green } from '@mui/material/colors';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import FavoriteButton from '../Favorite-button/FavoriteButton.jsx';
 
-const MovieCard = ({ posterUrl, title, genres, year, id }) => {
-  const splitedDate = year.split('-');
-  let month = 'января';
+const MovieCard = ({ posterUrl, title, genres = [], year, id }) => {
+  let formatedDate = '';
 
-  switch (splitedDate[1]) {
-    case '01':
-      month = 'января';
-      break;
-    case '02':
-      month = 'февраля';
-      break;
-    case '03':
-      month = 'марта';
-      break;
-    case '04':
-      month = 'апреля';
-      break;
-    case '05':
-      month = 'мая';
-      break;
-    case '06':
-      month = 'июня';
-      break;
-    case '07':
-      month = 'июля';
-      break;
-    case '08':
-      month = 'августа';
-      break;
-    case '09':
-      month = 'сентября';
-      break;
-    case '10':
-      month = 'октября';
-      break;
-    case '11':
-      month = 'ноября';
-      break;
-    case '12':
-      month = 'декабря';
-      break;
+  if (year) {
+    const splitedDate = year.split('-');
+    let month = 'января';
+
+    switch (month) {
+      case '01':
+        month = 'января';
+        break;
+      case '02':
+        month = 'февраля';
+        break;
+      case '03':
+        month = 'марта';
+        break;
+      case '04':
+        month = 'апреля';
+        break;
+      case '05':
+        month = 'мая';
+        break;
+      case '06':
+        month = 'июня';
+        break;
+      case '07':
+        month = 'июля';
+        break;
+      case '08':
+        month = 'августа';
+        break;
+      case '09':
+        month = 'сентября';
+        break;
+      case '10':
+        month = 'октября';
+        break;
+      case '11':
+        month = 'ноября';
+        break;
+      case '12':
+        month = 'декабря';
+        break;
+    }
+
+    formatedDate = `${splitedDate[2]} ${month} ${splitedDate[0]} г.`;
   }
-
-  let formatedDate = `${splitedDate[2]} ${month} ${splitedDate[0]} г.`;
 
   return (
     <>
@@ -57,6 +60,7 @@ const MovieCard = ({ posterUrl, title, genres, year, id }) => {
           <div>
             <Box component={'img'} src={posterUrl} />
             <Link
+              reloadDocument
               style={{ color: 'black', textDecoration: 'none' }}
               to={`/movies/${id}`}
             >
